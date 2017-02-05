010035dc01cdc7e44d3a2cf3fbd31b804d02023a
hicoldcat <hicoldcat@foxmail.com>
2017-02-05 23:39:40 +0800

    add some modules and momentlife components

---
+++ src/components/momentlife/MomentLife.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="momentlife">
+      <div class="box">
+          <div class="pic">
+
+          </div>
+          <div class="destination">
+
+          </div>
+          <div class="content">
+
+          </div>
+          <div class="owner">
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+</style>
