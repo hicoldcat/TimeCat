09c59de5aeeacbb39a3ec56885323897b0c2de07
hicoldcat <hicoldcat@foxmail.com>
2017-02-17 10:26:49 +0800

    complete daily module

--- src/modules/daily/Daily.vue
+++ src/modules/daily/Daily.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="daily">
     <v-header :nextTitle="nextTitle" :nextPage="nextPage"></v-header>
-    <h1>daily Modules</h1>
-    <h1>片刻留白 生活状态！</h1>
+    <div class="info-container" v-for="item in dailylist">
+      <info :info="item" class="info"></info>
+    </div>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import axios from 'axios'
 
 import Header from 'components/header/Header.vue'
+import Info from 'components/info/Info.vue'
 
 const ERR_OK = 0
 
@@ -17,23 +19,30 @@ export default {
   data () {
     return {
       nextTitle: "回音留白",
-      nextPage:"hearsensation"
+      nextPage:"hearsensation",
+      dailylist:[]
     }
   },
   created() {
-    // axios.get('/api/daily').then((res) => {
-    //   res = res.data
-    //   if (res.errno === ERR_OK) {
-    //     this.daily = res.data
-    //   }
-    // }).catch((error) => {
-    //   console.warn(error)
-    // })
+    axios.get('/api/daily').then((res) => {
+      res = res.data
+      if (res.errno === ERR_OK) {
+        this.dailylist = res.data
+      }
+    }).catch((error) => {
+      console.warn(error)
+    })
   },
   components: {
-    "v-header": Header
+    "v-header": Header,
+    "info": Info
   }}
 </script>
 
 <style lang="less">
+  .daily {
+    .info{
+      margin: 20px 0;
+    }
+  }
 </style>
