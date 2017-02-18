54b3934a3e20c7fdaf2d405c36ab7e566722e03c
hicoldcat <hicoldcat@foxmail.com>
2017-02-18 14:57:13 +0800

    抽取info组件的bottom为单独组件

--- src/components/info/Info.vue
+++ src/components/info/Info.vue
@@ -7,37 +7,23 @@
     <div class="desc">
       {{info.desc}}
     </div>
-    <div class="bottom">
-      <div class="time">
-        <div class="day">
-          Feb.02
-        </div>
-        <div class="year">
-          2017
-        </div>
-        <div class="plus">+</div>
-      </div>
-      <div class="owner">
-        <div class="avatar">
-          <img :src="info.avatar" alt="">
-        </div>
-        <div class="name">
-          {{info.nickname}}
-        </div>
-      </div>
-    </div>
+    <bottom-bar :info="info"></bottom-bar>
   </div>
 </div>
 </template>
 
 <script>
+import BottomBar from 'components/bottombar/BottomBar'
+
 export default {
   props: {
     info: {
       type: Object
     }
+  },
+  components: {
+    "bottom-bar": BottomBar
   }
-
 }
 </script>
 
@@ -63,8 +49,10 @@ export default {
         width: 90%;
         background-color: @baseBackgroundColor;
         margin: 0 auto;
-        border: 1px solid @baseBorderColor;
+        border: 1px solid #d1dbe5;
         margin: 0 auto;
+        box-shadow:0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
+        border-radius:4px;
 
         .pic {
           width: 94%;
@@ -83,55 +71,6 @@ export default {
           padding: 5% 0;
           width: 80%;
         }
-
-        .bottom{
-          margin: 3%;
-          display: flex;
-          justify-content: space-between;
-          align-items: flex-end;
-
-          .owner {
-            flex: 0 0 20%;
-            display: inline-flex;
-            flex-direction: column;
-            align-items: center;
-            vertical-align: bottom;
-
-            .avatar img{
-              border-radius: 50%;
-              width: 40px;
-              height: 40px;
-              margin: 0 auto;
-            }
-
-            .name {
-              line-height: 40px;
-              height: 40px;
-              font-size: 16px;
-              text-align: center;
-              font-family: fangzheng;
-              align-self: center;
-            }
-          }
-
-          .time {
-            flex: 0 0 20%;
-            display: inline-block;
-            border: 1px solid #B7B7B7;
-            color: #B7B7B7;
-            padding: 2px;
-            width: 20%;
-            height: 82px;
-            line-height: 20px;
-
-            .plus{
-              position: absolute;
-              bottom: 20px;
-              font-size: 20px;
-            }
-          }
-        }
-
 
     }
 }
