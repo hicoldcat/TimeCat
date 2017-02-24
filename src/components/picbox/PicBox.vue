559badc1ebd7cff3ba9e80a4b2d8a3019a06f3c3
hicoldcat <hicoldcat@foxmail.com>
2017-02-24 09:27:59 +0800

    add picbox component

---
+++ src/components/picbox/PicBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pic">
+    <img :src="src" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .pic {
+    width: 94%;
+    margin: 3%;
+
+    img{
+     width: 100%;
+   }
+  }
+</style>
