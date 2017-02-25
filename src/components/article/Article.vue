093cf6fdebb0b257b6a177b60cbeb77edc7583a6
hicoldcat <hicoldcat@foxmail.com>
2017-02-25 09:42:18 +0800

    add article component

---
+++ src/components/article/Article.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="article">
+    <div class="box">
+      <pic-box :src="article.pic"></pic-box>
+      <bottom-bar :nickname="article.nickname" :avatar="article.avatar" :time="article.time"></bottom-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+import PicBox from 'components/picbox/PicBox'
+import BottomBar from 'components/bottombar/BottomBar'
+
+
+export default {
+  props: {
+    article: {
+      type: Object
+    }
+  },
+  components: {
+    "bottom-bar": BottomBar,
+    "pic-box": PicBox
+  }
+}
+</script>
+
+<style lang="css">
+</style>
