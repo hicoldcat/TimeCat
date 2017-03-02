1a276c1f9f594c093efdbee521074920805ebc25
hicoldcat <hicoldcat@foxmail.com>
2017-03-02 13:01:23 +0800

    complete tarvels module

---
+++ src/components/trip/Trip.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="trip">
+    <div class="box">
+      <pic-box :src="trip.pic"></pic-box>
+      <div class="center">
+        <div class="destination">
+          {{'【'+trip.destination+'】'}}
+        </div>
+        <div class="title">
+          {{trip.title}}
+        </div>
+      </div>
+      <bottom-bar :nickname="trip.nickname" :avatar="trip.avatar" :time="trip.time"></bottom-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+import PicBox from 'components/picbox/PicBox'
+import BottomBar from 'components/bottombar/BottomBar'
+
+export default {
+  props: {
+    trip: {
+      type: Object
+    }
+  },
+  components: {
+    "bottom-bar": BottomBar,
+    "pic-box": PicBox
+  }
+}
+</script>
+
+<style lang="less">
+  .trip {
+      position: relative;
+      width: 100%;
+      background-color: #fff;
+
+      .box {
+          position: relative;
+          width: 90%;
+          background-color: #fff;
+          margin: 0 auto;
+          border: 1px solid #d1dbe5;
+          margin: 0 auto;
+          box-shadow:0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
+          border-radius:4px;
+
+          .center {
+            font-size: 16px;
+            font-weight: 400;
+            font-family: fangzheng;
+            margin:3%;
+            padding: 5% 0;
+            width: 94%;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            display: flex;
+
+            .destination {
+            }
+
+            .title {
+              flex: 1;
+              overflow: hidden;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+            }
+          }
+      }
+  }
+</style>
--- src/modules/travels/Travels.vue
+++ src/modules/travels/Travels.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="">
+  <div class="travels">
     <v-header :nextTitle="nextTitle" :nextPage="nextPage"></v-header>
-    <h1>travels Modules</h1>
-    <h1>半步留白 旅行！！</h1>
+    <div class="trip-container" v-for="item in triplist">
+      <trip :trip="item" class="trip"></trip>
+    </div>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import axios from 'axios'
 
 import Header from 'components/header/Header.vue'
+import Trip from 'components/trip/Trip.vue'
 
 const ERR_OK = 0
 
@@ -17,23 +19,30 @@ export default {
   data () {
     return {
       nextTitle: "此处留白",
-      nextPage:"home"
+      nextPage:"home",
+      triplist:[]
     }
   },
   created() {
-    // axios.get('/api/travels').then((res) => {
-    //   res = res.data
-    //   if (res.errno === ERR_OK) {
-    //     this.travelslist = res.data
-    //   }
-    // }).catch((error) => {
-    //   console.warn(error)
-    // })
+    axios.get('/api/travels').then((res) => {
+      res = res.data
+      if (res.errno === ERR_OK) {
+        this.triplist = res.data
+      }
+    }).catch((error) => {
+      console.warn(error)
+    })
   },
   components: {
-    "v-header": Header
+    "v-header": Header,
+    "trip": Trip
   }}
 </script>
 
 <style lang="less">
+  .travels {
+    .trip{
+      margin: 20px 0;
+    }
+  }
 </style>
