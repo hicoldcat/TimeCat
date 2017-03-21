d05bc0a7f8c70cade2739f7c79dbd9827a8b7fa9
hicoldcat <hicoldcat@foxmail.com>
2017-03-21 14:59:44 +0800

    remove slide bar

--- src/pages/choosetheme/ChooseTheme.vue
+++ src/pages/choosetheme/ChooseTheme.vue
@@ -1,15 +1,9 @@
 <template lang="html">
-  <div class="">
-    <side-bar></side-bar>
-  </div>
+
 </template>
 
 <script>
-import SideBar from 'components/sidebar/SideBar'
 export default {
-  components: {
-    "side-bar": SideBar
-  }
 }
 </script>
 
