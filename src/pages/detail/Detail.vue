7cb471b07b47231871bab9b9c0714b4a2a6c2eed
hicoldcat <hicoldcat@foxmail.com>
2017-04-17 16:48:00 +0800

    add detail page

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="detail">
+    <h1>Detail</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+
+  },
+  created () {
+    console.info(this.$route.params)
+  }
+}
+</script>
+
+<style lang="css">
+</style>
